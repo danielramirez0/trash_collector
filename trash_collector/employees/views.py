6642eeb247b3766e26b10b7f8df68cf79b0025fc
Daniel Ramirez <zoerunner@gmail.com>
2021-10-27 11:11:24 -0700

    Add assigned_customers query to context Update employee profile with proper parent template

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -4,25 +4,34 @@ from django.urls import reverse
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
 from django.apps import apps
+from django.db.models import Q
 from datetime import date
+
 
 from .models import Employee
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
+
 @login_required
 def index(request):
     logged_in_user = request.user
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
+    Customer = apps.get_model('customers.Customer')
+    today = date.today()
     try:
         logged_in_employee = Employee.objects.get(user=logged_in_user)
-        Customer = apps.get_model('customers.Customer')
-        today = date.today()
+        assigned_customers = Customer.objects.filter(
+            zip_code=logged_in_employee.zip_code
+        ).filter(Q(weekly_pickup=today.strftime('%A')) | Q(one_time_pickup=today)
+                 ).exclude(Q(suspend_start__lte=today), Q(suspend_end__gte=today)
+                           ).exclude(date_of_last_pickup=today)
 
         context = {
             'logged_in_employee': logged_in_employee,
-            'today': today
+            'today': today,
+            'assigned_customers': assigned_customers
         }
         return render(request, 'employees/index.html', context)
     except ObjectDoesNotExist:
@@ -35,11 +44,13 @@ def create(request):
     if request.method == "POST":
         name_from_form = request.POST.get('name')
         zip_from_form = request.POST.get('zip_code')
-        new_employee = Employee(name=name_from_form, user=logged_in_user, zip_code=zip_from_form)
+        new_employee = Employee(name=name_from_form,
+                                user=logged_in_user, zip_code=zip_from_form)
         new_employee.save()
         return HttpResponseRedirect(reverse('employees:index'))
     else:
         return render(request, 'employees/create.html')
+
 
 @login_required
 def edit_profile(request):
