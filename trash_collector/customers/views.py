7700a2e9e0c5b59708170fa6a9e209b6a3d6abfa
Daniel Ramirez <zoerunner@gmail.com>
2021-10-28 21:05:13 -0700

    Adds lat long to form submit for geolocation and map pin

--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -8,6 +8,7 @@ from django.conf import settings
 
 from .models import Customer
 
+
 @login_required
 def index(request):
     # The following line will get the logged-in user (if there is one) within any view function
@@ -17,7 +18,7 @@ def index(request):
         logged_in_customer = Customer.objects.get(user=logged_in_user)
 
         today = date.today()
-        
+
         context = {
             'logged_in_customer': logged_in_customer,
             'today': today
@@ -26,15 +27,30 @@ def index(request):
     except ObjectDoesNotExist:
         return HttpResponseRedirect(reverse('customers:create'))
 
+
 @login_required
 def create(request):
     logged_in_user = request.user
     if request.method == "POST":
         name_from_form = request.POST.get('name')
         address_from_form = request.POST.get('address')
+        city_from_form = request.POST.get('city')
         zip_from_form = request.POST.get('zip_code')
+        country_from_form = request.POST.get('country')
         weekly_from_form = request.POST.get('weekly_pickup')
-        new_customer = Customer(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form, weekly_pickup=weekly_from_form)
+        latitude_from_form = request.POST.get('latitude')
+        longitude_from_form = request.POST.get('longitude')
+        new_customer = Customer(
+            name=name_from_form,
+            user=logged_in_user,
+            address=address_from_form,
+            city=city_from_form,
+            zip_code=zip_from_form,
+            country=country_from_form,
+            weekly_pickup=weekly_from_form,
+            latitude=latitude_from_form,
+            longitude=longitude_from_form,
+            )
         new_customer.save()
         return HttpResponseRedirect(reverse('customers:index'))
     else:
@@ -42,6 +58,7 @@ def create(request):
             'api_key': settings.GOOGLE_API_KEY
         }
         return render(request, 'customers/create.html', context)
+
 
 @login_required
 def suspend_service(request):
@@ -60,6 +77,7 @@ def suspend_service(request):
         }
         return render(request, 'customers/suspend.html', context)
 
+
 @login_required
 def one_time_pickup(request):
     logged_in_user = request.user
@@ -75,6 +93,7 @@ def one_time_pickup(request):
         }
         return render(request, 'customers/one_time.html', context)
 
+
 @login_required
 def edit_profile(request):
     logged_in_user = request.user
@@ -82,12 +101,20 @@ def edit_profile(request):
     if request.method == "POST":
         name_from_form = request.POST.get('name')
         address_from_form = request.POST.get('address')
+        city_from_form = request.POST.get('city')
         zip_from_form = request.POST.get('zip_code')
+        country_from_form = request.POST.get('country')
         weekly_pickup_from_form = request.POST.get('weekly_pickup')
+        latitude_from_form = request.POST.get('latitude')
+        longitude_from_form = request.POST.get('longitude')
         logged_in_customer.name = name_from_form
         logged_in_customer.address = address_from_form
+        logged_in_customer.city = city_from_form
         logged_in_customer.zip_code = zip_from_form
+        logged_in_customer.country = country_from_form
         logged_in_customer.weekly_pickup = weekly_pickup_from_form
+        logged_in_customer.latitude = latitude_from_form
+        logged_in_customer.longitude = longitude_from_form
         logged_in_customer.save()
         return HttpResponseRedirect(reverse('customers:index'))
     else:
